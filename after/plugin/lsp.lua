2e0910af70330388388c306fb1a7bac6cac96ba1
scabbage <thescabbage@gmail.com>
2025-09-30 00:48:27 +0800

    Remove jdtls stubs

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,15 +1,13 @@
 vim.lsp.config('jdtls', {
   cmd = { 'jdtls' },
   filetypes = { 'java' },
-  capabilities = capabilities,
+  -- capabilities = capabilities,
   settings = {
     java = {
       signatureHelp = { enabled = true },
-      extendedClientCapabilities = extendedClientCapabilities,
+      -- extendedClientCapabilities = extendedClientCapabilities,
     },
   },
 })
 
 vim.lsp.enable 'jdtls'
-
--- comment
