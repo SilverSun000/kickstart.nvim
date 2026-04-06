4e07883455190d0adb2b917e9d299fa53e62aaeb
will <williamcahill78@gmail.com>
2026-04-06 17:04:33 -0500

    Updated to 0.12.1 and fixed some errors from the update

--- init.lua
+++ init.lua
@@ -950,7 +950,8 @@ require('lazy').setup({
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    -- (deprecated) main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    main = 'nvim-treesitter',
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'java' },
